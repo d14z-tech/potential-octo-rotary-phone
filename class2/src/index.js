4efd4d1fbb204f62c98cbc893216c3d5e7e45798
Juan Diaz <d14z.tech@gmail.com>
2023-03-11 13:32:15 -0500

    class2 refactor

--- class2/src/index.js
+++ class2/src/index.js
@@ -8,17 +8,24 @@ const readFile = ()=> {
     const filePath = path.resolve(`${__dirname}/archivo.txt`);
     const data = fs.readFileSync(filePath, 'binary');
     console.log(`reading file: ${filePath.split('/').pop()}`);
-    console.log(`file content: ${data}`);
+    console.log(`file content:\n${data}`);
   } catch(error) {
     console.log(error);
   }
 }
 
-const writeFile = (content)=> {
+const writeFile = (content, append = false)=> {
   try {
     const filePath = path.resolve(`${__dirname}/archivo.txt`);
     console.log(`writing in file: ${filePath.split('/').pop()}`);
-    fs.writeFileSync(filePath, content)
+
+    let formatted_content = `  ${content}\n`;
+
+    if(append) {
+      fs.appendFileSync(filePath, formatted_content);
+    } else {
+      fs.writeFileSync(filePath, formatted_content);
+    }
   } catch(error) {
     console.log(error);
   }
@@ -27,6 +34,6 @@ const writeFile = (content)=> {
 
 writeFile('hola mundo');
 readFile();
-writeFile(`the number 3 is odd: ${isOdd(3)}`)
+writeFile(`the number 3 is odd: ${isOdd(3)}`, true)
 readFile();
 fetchApi('https://rickandmortyapi.com/api/character');
--- class2/src/utils/api.js
+++ class2/src/utils/api.js
@@ -2,7 +2,10 @@ const fetchApi = async (url) => {
   try {
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data);
+    console.log(`Rick and Morty first ${Math.ceil(data.info.count/data.info.pages)} characters:\n`)
+    data.results.forEach(element => {
+      console.log(`  #${element.id} - ${element.name} is ${element.species}\n`);
+    });
   } catch (error) {
     console.log(error);
   }
